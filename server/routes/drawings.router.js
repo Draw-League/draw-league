ac050275ccc5e65ad29946f0a382024348a2c724
Noah Seo <sht6205@gmail.com>
2024-09-17 19:37:16 -0500

    Updated Take Photo page

--- server/routes/drawings.router.js
+++ server/routes/drawings.router.js
@@ -1,64 +1,51 @@
-
-const axios = require('axios')
+const axios = require('axios');
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 router.get('/', (req, res) => {
-    // GET route code here
-   const queryText = `
-      // querytext goes here
-      `
-    pool.query(queryText)
+  const queryText = 'SELECT * FROM drawings ORDER BY created_at DESC;';
+  pool.query(queryText)
     .then(result => {
       res.send(result.rows);
     })
     .catch(err => {
-      console.log(`GET ERROR MESSAGE HERE:`, err);
-      res.sendStatus(500)
-    })
-    
-  });
-  
-  /**
-   * POST route template
-   */
-  router.post('/', (req, res) => {
-    // POST route code here
-    const queryText = `
-    // querytext goes here
-    `
-    pool.query( queryText)
-    .then(() => res.sendStatus(201))
-    .catch((err) => {
-      console.log('POST ERROR MESSAGE HERE: ', err);
+      console.log(`Error fetching drawings:`, err);
       res.sendStatus(500);
     });
-  
-  });
-  
-  
-  /**
-   * PUT route template
-   */
-  router.put('/', (req, res) => {
-      // PUT route code here
-      const queryText = `
-      // querytext goes here
-      `
-    pool.query(queryText)
-    .then(result => {
-      res.send(result.rows);
-    })
-    .catch(err => {
-      console.log(`PUT ERROR MESSAGE HERE:`, err);
-      res.sendStatus(500)
-    })
-    });
-    
+});
 
+router.post('/', (req, res) => {
+  const { teamName, gameCode, imageUrl } = req.body;
+
+  const queryText = `
+    INSERT INTO drawings (team_name, game_code, image_url, created_at)
+    VALUES ($1, $2, $3, NOW()) RETURNING *;
+  `;
+  pool.query(queryText, [teamName, gameCode, imageUrl])
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.log('Error saving drawing:', err);
+      res.sendStatus(500);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { imageUrl } = req.body;
+  const queryText = 'UPDATE drawings SET image_url = $1 WHERE id = $2;';
+  pool.query(queryText, [imageUrl, req.params.id])
+    .then(() => res.sendStatus(200))
+    .catch(err => {
+      console.log(`Error updating drawing:`, err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
