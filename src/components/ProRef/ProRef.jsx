166f3167695564f23960f7d6aaf59965205792b1
Kalia Xiong <kaliaxyooj@gmail.com>
2024-09-23 18:32:16 -0500

    added useeffect and dispatch template to ProRef

--- src/components/ProRef/ProRef.jsx
+++ src/components/ProRef/ProRef.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
 import './ProRef.css';
+
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -7,8 +10,21 @@ import './ProRef.css';
 // or even care what the redux state is'
 
 function ProRef() {
+  const dispatch = useDispatch();
+  const {id} = useParams();
+  // const refDetails = useSelector(store => store.);
+  const history = useHistory();
+
+  useEffect(() => {
+    dispatch({ type: "INSERT SAGA DISPATCH HERE", payload: id });
+  }, [id, dispatch]);
+
+
   return (
+
+    
     <div className="container">
+      {/* {refDetails.map(ref => {})} */}
       <div className='ref-tite'>
       <h2 className='ref-title'> REFEREE</h2>
       </div>
@@ -32,25 +48,7 @@ function ProRef() {
         <p> @instagram/social media handle </p>
       </div>
 
-      <div className='ref-info'>
-      <div className='ref-image'>
-        <p>Ref Image</p>
-      </div>
-
-      <div className='ref-name'>
-       <p>Ref Name</p>
-      </div>
-
-      <div className='ref-facts'>
-        <p className='ref-style'>occupation</p>
-        <p className='ref-style'>art medium</p>
-        <p className='ref-style'>fun fact</p>
-      </div>
-      </div>
-      <div className='ref-social-media'>
-        <p> @instagram/social media handle </p>
-      </div>
-    
+      
     </div>
 
     
