60309eca913a031b263db4c2ca25d639c5efac77
Kalia Xiong <kaliaxyooj@gmail.com>
2024-09-25 00:34:02 -0500

    updates a little bit of proRef CSS, created UI for  ProJudge view- still need to add drop shadow, need to add buttons for both pages as well as update CSS ro enlarge proRef ui for visibility

--- src/components/ProJudge/ProJudge.jsx
+++ src/components/ProJudge/ProJudge.jsx
@@ -1,20 +1,53 @@
 import React from 'react';
 import './ProJudge.css';
 
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
+
 
 function ProJudge() {
   return (
     <div className="container">
-      <div>
-        <p>This the projector judge page</p>
-        <p>This will get</p>
-      </div>
-    </div>
-  );
-}
+          <div className='judge-title'>
+            <h2 className='jtitle-style'> JUDGE</h2>
+          </div>
+          <section className="judge-info">
+          
+                {/* <div key={judge.id}>  */}
+                
+       
+                  <div className='judge-image'>
+                   <p> JUDGE IMAGE</p>
+                  </div>
+    
+                  <div className='judge-name'>
+                    <p>JUDGE NAME</p>
+                  </div>
+
+                  
+                  <div className='judge-question-one'>
+                    <h3 className='question-style'>WHAT DO YOU DO?</h3>
+                  </div>
+                  <div className='judge-answer'>
+                    <h3 className='answer-style'>Answer</h3>
+                  </div>
+
+                  <div className='judge-question-two'>
+                    <h3 className='question-style'>WHAT DO YOU LIKE?</h3>
+                  </div>
+                  <div className='judge-answer-two'>
+                    <h3 className='answer-style'>Answer two</h3>
+                  </div>
+
+                  <div className='judge-question-three'>
+                    <h3 className='question-style'>WHAT DO YOU KNOW?</h3>
+                  </div>
+                  <div className='judge-answer-three'>
+                    <h3 className='answer-style'>Answer three</h3>
+                  </div>
+                {/* </div> */}
+   
+          </section>
+        </div>
+      );
+    }
 
 export default ProJudge;
