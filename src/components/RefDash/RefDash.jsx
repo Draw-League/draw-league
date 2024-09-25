bab5963ac7f4a2e5ea8f598c077f6765eb1d5bd9
Conor Callahan <conorcallahan97@outlook.com>
2024-09-24 23:59:19 -0500

    Imported the logo.png and have started styling and planning for how to establish the dashboard and its requirements.

--- src/components/RefDash/RefDash.jsx
+++ src/components/RefDash/RefDash.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import './RefDash.css';
 import Timer from './ProTimer';
+import logo from './drawleague.png';
 
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is'
-
+{/* This is the ref dashboard
+This will get, post, and put */}
 function RefDash() {
   return (
     <div className="dashboard-container">
-      <div>
-        <p>This is the ref dashboard</p>
-        <p>This will get, post, and put</p>
-        <div className="timer-div">
+      <div className='top-row'>
+       <div className="timer">
           <Timer/>
         </div>
-
-      </div>
+        </div>
+        <div className='body-row'>
+        <div className='logo-container'>
+          <img src={logo} alt="DRAW LEAUGE LOGO" className='logo'/>
+        </div>
+        </div>
     </div>
   );
 }
