85854a9b83e42edfa16218548f10628061ac89f8
Kalia Xiong <kaliaxyooj@gmail.com>
2024-09-26 16:40:52 -0500

    deleted unnec. texts from proRef

--- src/components/ProRef/ProRef.jsx
+++ src/components/ProRef/ProRef.jsx
@@ -42,7 +42,6 @@ function ProRef() {
 
               <div className='ref-facts'>
               <p className='ref-style'>ref facts</p>
-              <p className='ref-style'>rdufharknfdkjhaouhrtmnchujfajhfja kjguhna vfhbg jhguerhgajdfghs</p>
                 {/* <p className='ref-style'>{ref.ref_job}</p>
                 <p className='ref-style'>{ref.fact}</p>
                 <p className='ref-style'>{ref.art_medium}</p> */}
