79d9f729d509c616c05678482691a7df421a7ea0
Kalia Xiong <kaliaxyooj@gmail.com>
2024-09-26 12:44:56 -0500

    updated projudge css

--- src/components/ProJudge/ProJudge.jsx
+++ src/components/ProJudge/ProJudge.jsx
@@ -9,20 +9,22 @@ function ProJudge() {
           <div className='judge-title'>
             <h2 className='jtitle-style'> JUDGE</h2>
           </div>
-          <section className="judge-info">
-          
-                {/* <div key={judge.id}>  */}
-                
+
+          <div className="judge-info">
        
+       <div className='name-info'>
                   <div className='judge-image'>
-                   <p> JUDGE IMAGE</p>
+                   
+                   <img alt="placeholder image" 
+                   src ='https://images.unsplash.com/photo-1565194637906-8f45f3351a5d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
                   </div>
     
                   <div className='judge-name'>
                   <h3 className='question-style'>JUDGE NAME</h3>
                   </div>
+        </div>
 
-                  
+           <div className ="questions">     
                   <div className='judge-question-one'>
                     <h3 className='question-style'>WHAT DO YOU DO?</h3>
                   </div>
@@ -43,9 +45,9 @@ function ProJudge() {
                   <div className='judge-answer-three'>
                     <h3 className='answer-style'>Answer three</h3>
                   </div>
-                {/* </div> */}
+                  </div>  
    
-          </section>
+          </div>
         </div>
       );
     }
