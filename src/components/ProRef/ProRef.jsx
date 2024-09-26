c0ef8f5f48433aea9a813e64d7ee78e4fead9af1
Kalia Xiong <kaliaxyooj@gmail.com>
2024-09-26 16:40:01 -0500

    updated proRef styling

--- src/components/ProRef/ProRef.jsx
+++ src/components/ProRef/ProRef.jsx
@@ -25,18 +25,24 @@ function ProRef() {
       <div className='ref-title'>
         <h2 className='ref-title-style'> REFEREE</h2>
       </div>
-      <section className="ref-info">
+      <div className="ref-info">
         {/* {refDetails.map((ref) => (  */}
             {/* <div key={ref.id}>  */}
               <div className='ref-image'>
                {/* <p> {ref.ref_img}</p> */}
+               <img alt="placeholder image" 
+                   src ='https://images.unsplash.com/photo-1565194637906-8f45f3351a5d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
+              
               </div>
 
               <div className='ref-name'>
                 {/* <p>{ref.full_name}</p> */}
+                <p className='rname-style'>ref name</p>
               </div>
 
               <div className='ref-facts'>
+              <p className='ref-style'>ref facts</p>
+              <p className='ref-style'>rdufharknfdkjhaouhrtmnchujfajhfja kjguhna vfhbg jhguerhgajdfghs</p>
                 {/* <p className='ref-style'>{ref.ref_job}</p>
                 <p className='ref-style'>{ref.fact}</p>
                 <p className='ref-style'>{ref.art_medium}</p> */}
@@ -44,7 +50,10 @@ function ProRef() {
             {/* </div> */}
           {/* ))
         } */}
-      </section>
+       
+      </div> 
+      
+
     </div>
   );
 }
