4a50972be569b52c686ba0c2ae2658970813ed77
Kalia Xiong <kaliaxyooj@gmail.com>
2024-09-28 10:04:34 -0500

    updated some ref styling

--- src/components/ProRef/ProRef.jsx
+++ src/components/ProRef/ProRef.jsx
@@ -9,45 +9,45 @@ function ProRef() {
   const dispatch = useDispatch();
   const { id } = useParams();
   const ref = useSelector((store) => store.projectionReducer);
-  const history = useHistory(); 
+  const history = useHistory();
 
   useEffect(() => {
     dispatch({ type: "FETCH_REFS", payload: id });
   }, [id, dispatch]);
 
- 
+
 
   return (
-<div className="container">
+    <div className="container">
       <div className='ref-title'>
         <h2 className='ref-title-style'> REFEREE</h2>
       </div>
       <div className="ref-info">
-      
-            <div key={ref.id}> 
-              <div className='ref-image'>
-              
-               <img className = "ref-img" alt="placeholder image" 
-                   src ='https://images.unsplash.com/photo-1565194637906-8f45f3351a5d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
-              
-              </div>
 
-              <div className='ref-name'>
-                
-                <p className='rname-style'>ref name</p>
-              </div>
+        <div key={ref.id}>
+          <div className='ref-image'>
 
-              <div className='ref-facts'>
-              <p className='ref-style'>ref facts</p>
-                <p>ref facts</p>
-              </div>
-           
-        
-        
-      </div> 
-      <div className='ref-social-media'>@instagram handle</div>
+            <img className="ref-img" alt="placeholder image"
+              src='https://images.unsplash.com/photo-1565194637906-8f45f3351a5d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
 
-    </div>
+          </div>
+
+          <div className='ref-name'>
+            <p className='rname-style'>ref name</p>
+          </div>
+
+          <div className='ref-facts'>
+            <p className='ref-style'>
+              ref facts
+              <br />
+              how much wood would a woodchuck chuck if a woodchuck could chuck wood?
+              </p>
+              <br /> 
+
+            <p className='ref-style'>@instagram handle</p> 
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
