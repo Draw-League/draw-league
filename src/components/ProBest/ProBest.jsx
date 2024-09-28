ca5a7fd1d36eaffb29bedd8d5e5876d3b32de348
Patrick Davison <davisonpatrickj@gmail.com>
2024-09-28 18:10:09 -0500

    winners router work

--- src/components/ProBest/ProBest.jsx
+++ src/components/ProBest/ProBest.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './ProBest.css';
+import {useSelector, useDispatch} from 'react-redux';
+import { useEffect } from 'react';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -7,10 +9,30 @@ import './ProBest.css';
 // or even care what the redux state is'
 
 function ProBest() {
+
+  const winners = useSelector((store) => store.adminDashReducer);
+  const dispatch = useDispatch();
+
+
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_WINNERS' });
+  }, [dispatch]);
+
   return (
     <div className="container">
       <div>
         <p>This the projector best drawing</p>
+        <section className="ref-info">
+        {winners.map((winner) => ( 
+            <div key={winner.id}> 
+              <div className='ref-image'>
+               <p> {winner.location_name}</p>
+              </div>
+            </div>
+          ))
+        }
+      </section>
         <p>This will get</p>
       </div>
     </div>
