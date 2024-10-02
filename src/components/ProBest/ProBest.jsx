a40c9d7b4ffee7185cc8cbf7f98b110d615af142
Patrick Davison <davisonpatrickj@gmail.com>
2024-10-02 08:44:28 -0500

    probest working until displaying data

--- src/components/ProBest/ProBest.jsx
+++ src/components/ProBest/ProBest.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ProBest.css';
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
@@ -9,51 +9,52 @@ import { useHistory } from 'react-router-dom';
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is'
 
-function ProBest({socket}) {
+function ProBest({ socket }) {
 
-const dispatch = useDispatch();
-    const ref = useSelector((store) => store.projectionReducer);
-    const currentGame = useSelector((store) => store.currentGame)
+  const dispatch = useDispatch();
+  const ref = useSelector((store) => store.projectionReducer);
+  const currentGame = useSelector((store) => store.currentGame)
+  const winners = useSelector((store) => store.winnersReducer);
+  const history = useHistory();
+  const eventId = currentGame.event_id
+  const favoriteWinner = winners.find(winner => winner.favorite_drawing === true);
+  console.log(favoriteWinner);
 
-    const history = useHistory();
-  
-    const eventId = currentGame.event_id
-    
-   useEffect(() => {
+
+  useEffect(() => {
     dispatch({ type: 'FETCH_WINNERS', payload: eventId });
   }, [dispatch, eventId]);
-  
-    useEffect(() => {
-      if (socket) {
-        const handleNavigation = (direction, currentGameIn) => {
-          console.log(`Navigating to: ${direction}`);
-          console.log(`currentGame:`, currentGameIn);
-          if(direction === 'next') {
-            history.push('/ProContactUs'); 
-          }
-          else if(direction === 'back') {
-            history.push('/ProWinners_3');
-          }
-        };
-  
-        socket.on('navigate', handleNavigation);
-        console.log('socket.id', socket.id);
-        return () => {
-          socket.off('navigate', handleNavigation);
-        };
-      }
-    }, [socket, history]);
 
-  const winners = useSelector((store) => store.winnersReducer);
-  
-  
+  useEffect(() => {
+    if (socket) {
+      const handleNavigation = (direction, currentGameIn) => {
+        console.log(`Navigating to: ${direction}`);
+        console.log(`currentGame:`, currentGameIn);
+        if (direction === 'next') {
+          history.push('/ProContactUs');
+        }
+        else if (direction === 'back') {
+          history.push('/ProWinners_3');
+        }
+      };
 
- 
+      socket.on('navigate', handleNavigation);
+      console.log('socket.id', socket.id);
+      return () => {
+        socket.off('navigate', handleNavigation);
+      };
+    }
+  }, [socket, history]);
 
-console.log('Drawings from db:', winners)
 
-  const favoriteWinner = winners.find(winner => winner.favorite_drawing === true);
-console.log(favoriteWinner);
+
+
+
+
+
+  console.log('Drawings from db:', winners)
+
+
   return (
     <div className="winners">
       <div className="winners-top">
@@ -74,17 +75,17 @@ console.log(favoriteWinner);
         <div
           className="winner"
           style={{
-            
+
           }}
         >
           <div className="winner-drawing">
-            <img className="best-img" src={favoriteWinner.drawing_url} alt="" />
+            {/* <img className="best-img" src={favoriteWinner.drawing_url} alt="" /> */}
           </div>
-          <div className="winner-name">{favoriteWinner.team_name}</div>
-         
+          {/* <div className="winner-name">{favoriteWinner.team_name}</div> */}
+
         </div>
       </div>
-      
+
     </div>
   );
 }
