3d416950fc54f75862acb413850646eba4288695
Noah Seo <sht6205@gmail.com>
2024-10-02 00:34:19 -0500

    changed Log out button location

--- src/components/AdminNav/AdminNav.jsx
+++ src/components/AdminNav/AdminNav.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './AdminNav.css';
 import { useHistory } from 'react-router-dom';
 import logo from '../LandingPage/drawleague.png';
+import LogOutButton from '../LogOutButton/LogOutButton';
 
 function AdminNav() {
   const history = useHistory();
+
+  const handleLogout = () => {
+    history.push('/home');
+  };
 
   const navigation = (path) => {
     history.push(path);
@@ -17,6 +22,7 @@ function AdminNav() {
         <ul className="admin-nav-menu">       
           <li><button className="admin-nav-button" onClick={() => navigation('/admindash')}>DASHBOARD</button></li>
           <li><button className="admin-nav-button" onClick={() => navigation('/addevent')}>CREATE EVENT</button></li>
+          <li><LogOutButton className="admin-nav-button" onLogout={handleLogout} /></li>
           {/* <li><button className="nav-button" onClick={() => navigation('/addref')}>ADD A REF</button></li> */}
         </ul>
       
