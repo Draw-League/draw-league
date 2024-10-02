8e3de0f5cd9c38caa29fd9dc739389d97b33f8f6
Patrick Davison <davisonpatrickj@gmail.com>
2024-10-02 00:18:12 -0500

    comment out projection sagas

--- server/routes/projections.router.js
+++ server/routes/projections.router.js
@@ -5,15 +5,14 @@ const router = express.Router();
 /**
  * GET Refs-intro
  */
-router.get('/ref-intro/:id', (req, res) => {
+router.get('/ref-intro', (req, res) => {
     const eventId = req.params.id;
     console.log('GET all REF reqparams.id:', req.params.id);
     const queryText = `
     SELECT art_medium, ref_img, full_name, ref_job, ref_fact, judge_name, judge_job, judge_like, judge_know, judge_img 
 FROM "user"
 JOIN user_event ON user_event.user_id = "user".id
-JOIN event ON user_event.event_id = event.id 
-WHERE event.id = $1;
+JOIN event ON user_event.event_id = event.id;
     `
     pool.query(queryText, [eventId])
         .then(result => {
